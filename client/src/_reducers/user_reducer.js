d49e9390fb1accb0faccd74f377078ab1eb10c85
kth7024 <guinness5167@gmail.com>
2022-01-12 09:21:25 +0900

    boiler-plate

---
+++ client/src/_reducers/user_reducer.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-anonymous-default-export */
+import {
+    LOGIN_USER,
+} from '../_actions/types';
+
+
+export default function (state = {}, action) {
+    switch (action.type) {
+        case LOGIN_USER:
+            return { ...state, loginSuccess: action.payload }
+
+        default:
+            return state;
+    }
+}
