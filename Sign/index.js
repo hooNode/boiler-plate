9249729b7643f066e965ba8883b10b8850629d29
kth7024 <guinness5167@gmail.com>
2022-04-07 17:31:22 +0900

    user model and schema clear

--- Sign/index.js
+++ Sign/index.js
@@ -2,6 +2,20 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://guinness5167:s2941208@boiler-project-sign.emgtm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    // {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    //   useCreateIndex: true,
+    //   useFindAndModify: false,
+    // }
+  )
+  .then(() => console.log("Mongoose Connected"))
+  .catch((err) => console.log(err));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
