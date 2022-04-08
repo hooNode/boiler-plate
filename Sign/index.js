dc9c676f8b85d60e1e3ed46702743a7a867977c0
kth7024 <guinness5167@gmail.com>
2022-04-08 15:36:15 +0900

    success bcrypt password

--- Sign/index.js
+++ Sign/index.js
@@ -25,7 +25,6 @@ app.get("/", (req, res) => {
 app.post("/register", (req, res) => {
   // 회원 가입 할 때 필요한 정보들을 client에서 가져오면 이것들을 DB에 넣어놓는다.
   const user = new User(req.body);
-  console.log(user);
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, message: err.message });
     return res.status(200).json({ success: true, message: userInfo });
--- Sign/models/user.js
+++ Sign/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -25,6 +27,23 @@ const userSchema = mongoose.Schema({
   tokenExp: {
     type: Number,
   },
+});
+
+// User 모델에 유저 정보를 저장하기 전에 다음 스크립트가 실행된다.
+userSchema.pre("save", function (next) {
+  var user = this;
+  // 비밀번호를 암호화 시킨다.
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        console.log("비밀번호가 성공적으로 암호화되었습니다.");
+        next();
+      });
+    });
+  }
 });
 
 const User = mongoose.model("User", userSchema);
